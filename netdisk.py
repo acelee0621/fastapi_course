137888139ba270da8e79018a7d95aa4cc8189f5f
acelee0621 <acelee0621@outlook.com>
2025-02-04 00:13:55 +0800

    RBAC

--- netdisk.py
+++ netdisk.py
@@ -6,11 +6,13 @@ from fastapi import (
     Query,
     Request,
     Response,
+    Security,
     status,
     UploadFile,
     Form,
 )
 from fastapi.responses import FileResponse, JSONResponse
+from fastapi.security import SecurityScopes
 
 from utils import unique_generator, save_files, UPLOAD_DIR
 from FakeDB import file_db
@@ -19,19 +21,68 @@ from FakeDB import file_db
 router = APIRouter(prefix="/netdisk", tags=["NetDisk"])
 
 
-def check_user(netdisk_token: str | None = Cookie(default=None)):
-    print(netdisk_token)
-    if netdisk_token != "Ace":
+ALL_USERS = {
+    "jack": ["admin", "users"],
+    "rose": ["admin", "users"],
+    "tom": ["users"],
+    "jerry": ["users"],
+}
+
+ROLE_PERMISSIONS = {
+    "admin": ["upload"],
+    "users": ["visit", "download"],
+}
+
+
+def get_user_token(user_name: str | None = Cookie(default=None)):
+    if user_name is None:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="You have no authorization"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="user_name is required"
         )
-    return netdisk_token
+    return user_name
 
 
-@router.post("/upload_file", summary="Upload a file")
-async def upload_file(
-    *, file: UploadFile, request: Request, token: str = Depends(check_user)
+# 返回角色(role)对应的所有权限(scopes)
+def get_role_permissions(role_name: list[str]):
+    permissions = []
+    for role in role_name:
+        for permission in ROLE_PERMISSIONS[role]:
+            permissions.append(permission)
+    return permissions
+
+
+# 获取用户所有的scopes,也就是获取角色对应的所有权限
+def get_user_permissions(token: str = Depends(get_user_token)):
+    if token in ALL_USERS:
+        return get_role_permissions(ALL_USERS[token])
+    return None
+
+
+def check_user(
+    security_scopes: SecurityScopes,
+    user_permission: str = Depends(get_user_permissions),
 ):
+    for scope in security_scopes.scopes:
+        # 检查路径接口需要的权限是否在用户对应的所有权限里
+        if scope not in user_permission:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You have no authorization",
+            )
+
+
+@router.get("/login", summary="Login")
+async def set_cookie(response: Response, token: str):
+    response.set_cookie(key="user_name", value=token, expires=600)
+    return {"message": "Cookie set Successful"}
+
+
+@router.post(
+    "/upload_file",
+    summary="Upload a file",
+    dependencies=[Security(check_user, scopes=["upload"])],
+)
+async def upload_file(*, file: UploadFile, request: Request):
     # 保存文件
     unique_name = await save_files(file)
     # 将文件信息保存到数据库
@@ -48,7 +99,11 @@ async def upload_file(
     }
 
 
-@router.get("/share", summary="Get all shared file")
+@router.get(
+    "/share",
+    summary="Get all shared file",
+    dependencies=[Security(check_user, scopes=["visit"])],
+)
 async def share_file(request: Request):
     all_files = file_db.get_all_files()
     data = {"all_files": all_files}
@@ -57,7 +112,11 @@ async def share_file(request: Request):
     )
 
 
-@router.get("/file/{unique_name}", summary="File Downing Page")
+@router.get(
+    "/file/{unique_name}",
+    summary="File Downing Page",
+    dependencies=[Security(check_user, scopes=["visit"])],
+)
 async def file_page(
     request: Request,
     unique_name: str,
@@ -83,7 +142,11 @@ async def file_page(
     )
 
 
-@router.post("/download/{unique_name}", summary="Download File")
+@router.post(
+    "/download/{unique_name}",
+    summary="Download File",
+    dependencies=[Security(check_user, scopes=["download"])],
+)
 async def download_file(unique_name: str, share: str = Form()):
     code = str(file_db.get_share_code(unique_name), "utf-8")
     if code != share:
@@ -99,5 +162,5 @@ async def download_file(unique_name: str, share: str = Form()):
 
 @router.get("/set-cookie", summary="Set Cookie")
 async def set_cookies(response: Response):
-    response.set_cookie(key="netdisk_token", value="Ace",expires=60)
+    response.set_cookie(key="netdisk_token", value="Ace_admin", expires=60)
     return {"message": "Cookie Set"}
