287ec97ccd973bfdbb7c4049647f9a73c79624ff
acelee0621 <acelee0621@outlook.com>
2025-02-03 10:55:35 +0800

    main

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+<<<<<<< Updated upstream
 from fastapi import FastAPI
+=======
+import sys
+
+from fastapi import FastAPI, status, Response
+from fastapi import __version__ as fastapi_version
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+
+>>>>>>> Stashed changes
 
 
 app = FastAPI()
@@ -8,3 +19,13 @@ app = FastAPI()
 async def run_state():
     return {"Status": "OK!"}
 
+<<<<<<< Updated upstream
+=======
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("tmp/logo.png")
+
+
+
+>>>>>>> Stashed changes
