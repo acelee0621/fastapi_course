ace3f27e8780c2770f0be32d6e7ba2704f1ef66c
acelee0621 <acelee0621@outlook.com>
2025-02-03 11:05:21 +0800

    main

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-from fastapi import FastAPI
-=======
 import sys
 
 from fastapi import FastAPI, status, Response
@@ -9,23 +6,47 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 
->>>>>>> Stashed changes
 
 
-app = FastAPI()
+app = FastAPI(
+    # docs_url=None,
+    # redoc_url=None
+)
+app.mount("/sub", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-async def run_state():
-    return {"Status": "OK!"}
+@app.get("/server-status", include_in_schema=False)
+async def run_state(response: Response, token: str | None = None):
+    if token == "Ace":
+        data = {
+            "Status": "OK!",
+            "fastapi_version": fastapi_version,
+            "python_version": sys.version_info,
+        }
+        return data
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND  # 404
+        return {"detail": "Not Found"}
 
-<<<<<<< Updated upstream
-=======
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse("tmp/logo.png")
 
 
+son_app = FastAPI()
 
->>>>>>> Stashed changes
+
+@son_app.get("/")  # /son/
+async def root():
+    return {"message": "I'm the son app!"}
+
+
+@son_app.get("/info")  # /son/info
+async def info():
+    return {"message": "I'm the son app info!"}
+
+
+app.mount("/son", son_app, name="son")
+
+
